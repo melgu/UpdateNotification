50c14be0616e6b15d6bd202710063c04dae8507e
Melvin Gundlach <mail@melvin-gundlach.de>
2020-05-08 19:13:55 +0200

    Change checkForUpdate. Add functions to show views.

--- Sources/UpdateNotification/UpdateNotification.swift
+++ Sources/UpdateNotification/UpdateNotification.swift
@@ -7,32 +7,69 @@ class UpdateNotification {
 		self.updateFeed = UpdateFeedManager(feedUrl: feedUrl)
 	}
 	
-	func checkForUpdates(currentVersion: String, currentBuild: String) {
+	func checkForUpdates(currentVersion: String, currentBuild: String) -> Bool {
 		updateFeed.load()
 		
 		guard let feed = updateFeed.feed else {
 			print("Feed unavailable.")
-			return
+			return false
 		}
 		
 		guard let lastItem = feed.items.last else {
 			print("No items in feed.")
-			return
+			return false
 		}
 		
 		if lastItem.version > currentVersion {
 			print("New version available.")
-			return
+			return true
 		}
 		
 		if let itemBuild = lastItem.build {
 			if lastItem.version == currentVersion &&
 				itemBuild > currentBuild {
 				print("New version available.")
-				return
+				return true
 			}
 		}
 		
 		print("No newer version available.")
+		return false
+	}
+	
+	@available(OSX 10.15, *)
+	func showNewVersionView() {
+		guard let lastItem = updateFeed.feed?.items.last else {
+			print("Feed unavailable")
+		}
+		
+		var currentVersion = "Unknown"
+		var currentBuild = "Unknown"
+		if let infoDict = Bundle.main.infoDictionary {
+			currentVersion = infoDict["CFBundleShortVersionString"] as? String ?? "Unknown"
+			currentBuild = infoDict["CFBundleVersion"] as? String ?? "Unknown"
+		}
+		
+		let controller = ResizableWindowController(rootView:
+			NewVersionView(item: lastItem, currentVersion: currentVersion, currentBuild: currentBuild)
+		)
+		controller.window?.title = "New version available"
+		controller.showWindow(nil)
+	}
+	
+	@available(OSX 10.15, *)
+	func showChangelogWindow() {
+		updateFeed.load()
+		
+		guard let items = updateFeed.feed?.items else {
+			print("Feed unavailable")
+			return
+		}
+		
+		let controller = ResizableWindowController(rootView:
+			FeedView(items: items)
+		)
+		controller.window?.title = "Changelog"
+		controller.showWindow(nil)
 	}
 }

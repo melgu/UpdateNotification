10d8b46a06b1d77871a87608aeb5303e2e25752d
Melvin Gundlach <mail@melvin-gundlach.de>
2020-05-09 03:20:26 +0200

    Change Codable.

--- Sources/UpdateNotification/Codables.swift
+++ Sources/UpdateNotification/Codables.swift
@@ -7,13 +7,23 @@ struct Feed: Codable {
 
 public struct Item: Codable, Identifiable, Comparable {
 	public var id: String { "\(version) \(String(describing: build))" }
-	public var version: String
-	public var build: String?
-	public var date: Date?
-	public var title: String?
-	public var text: String?
-	public var infoUrl: URL?
-	public var downloadUrl: URL?
+	var version: String
+	var build: String?
+	var date: Date?
+	var title: String?
+	var text: String?
+	var infoUrl: URL?
+	var downloadUrl: URL?
+	
+	public init(version: String, build: String? = nil, date: Date? = nil, title: String? = nil, text: String? = nil, infoUrl: URL? = nil, downloadUrl: URL? = nil) {
+		self.version = version
+		self.build = build
+		self.date = date
+		self.title = title
+		self.text = text
+		self.infoUrl = infoUrl
+		self.downloadUrl = downloadUrl
+	}
 	
 	public static func < (lhs: Item, rhs: Item) -> Bool {
 		if lhs.version == rhs.version &&
